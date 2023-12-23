1e8cba9355a79552dadf73ee82592d0cf50e5748
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-12-23 13:55:12 -0300

    go to initial state

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <input type="text" v-model="searchInput" class="w-2/4 outline-none shadow rounded-sm px-3 py-1">
         <button @click="searchByQuery" class="bg-teal-300 w-1/4 p-1 rounded-sm">Search</button>
     </section>
-    <main class="grid md:grid-cols-4 sm:grid-cols-2 xs-grid-cols-1 max-w-5xl mx-auto gap-4 my-6" ref="el">
+    <main class="grid md:grid-cols-4 sm:grid-cols-2 xs-grid-cols-1 max-w-5xl mx-auto gap-4 my-6 min-h-full overflow-y-auto">
         <div v-for="e in results" :key="e.id" class="shadow rounded-sm overflow-hidden">
             <img :src="e.images.downsized_medium.url" :alt="e.title">
             <p class="text-center my-2">{{ e.title }}</p>
@@ -16,8 +16,7 @@
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue'
-    import { useInfiniteScroll } from '@vueuse/core'
+    import { onMounted, ref } from 'vue'
     import GiphyAPI from './services/giphy.ts'
 
     const giphy = new GiphyAPI()
@@ -25,19 +24,12 @@
     const results = giphy.searched
     const searchInput = ref('')
     const endpoint = ref('gifs')
-    const el = ref<HTMLElement | null>(null)
-    const currentPage = ref(0)
 
     const searchByQuery = () =>{
         giphy.fetchSearch(endpoint.value, searchInput.value)
     }
 
-    useInfiniteScroll(
-        el,
-        async () => {
-            currentPage.value++
-            await giphy.fetchOnMounted(currentPage.value)
-        },
-        { distance: 50 }
-    )
+    onMounted(async() =>{
+        await giphy.fetchOnMounted()
+    })
 </script>
